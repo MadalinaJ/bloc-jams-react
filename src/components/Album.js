0ff0a976e98249c4e6a042e72bbe6b7dc9b3c1c9
Madalina Jantea <madalina.jantea@gmail.com>
2019-04-18 00:25:08 -0700

    Library.js and Album.js modif key

--- src/components/Album.js
+++ src/components/Album.js
@@ -18,7 +18,7 @@ class Album extends Component {
     return (
       <section className="album">
         <section id="album-info">
-          <img id="album-cover-art" src={this.state.album.albumCover} />
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
@@ -35,7 +35,7 @@ class Album extends Component {
           <tbody>
           {
             this.state.album.songs.map(( song, index ) =>
-              <tr>
+              <tr key = {index}>
                 <td className="song-number">{index+1}</td>
                 <td className="song-title">{song.title}</td>
                 <td className="song-duration">{song.duration}</td>
--- src/components/Library.js
+++ src/components/Library.js
@@ -13,7 +13,7 @@ import albumData from './../data/albums';
        <section className='library'>
        {
         this.state.albums.map( (album, index) =>
-         <Link to={`/album/${album.slug}`} >
+         <Link to={`/album/${album.slug}`} key={index}>
           <img src={album.albumCover} alt={album.title} />
              <div>{album.title}</div>
              <div>{album.artist}</div>
